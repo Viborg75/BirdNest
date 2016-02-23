a1e3885679875385bf77183aec27ad7ce5097183
pr0te <prote1337@gmail.com>
2016-02-23 13:36:45 +0100

    RGB LED support!

--- Scripts/MultipleServoTest/MultipleServoTest.ino
+++ Scripts/MultipleServoTest/MultipleServoTest.ino
@@ -1,6 +1,9 @@
 #include <Servo.h>
 
 Servo servo1, servo2, servo3, servo4;
+int redPin = 12;
+int greenPin = 13;
+int bluePin = 11;
 
 void setup(){
     // Pin setup
@@ -8,9 +11,19 @@ void setup(){
   	servo2.attach(5);
   	servo3.attach(6);
   	servo4.attach(7);
+    pinMode(redPin, OUTPUT);
+    pinMode(greenPin, OUTPUT);
+    pinMode(bluePin, OUTPUT);
+}
+
+void setColor(int red, int green, int blue){
+    analogWrite(redPin, red);
+    analogWrite(greenPin, green);
+    analogWrite(bluePin, blue);
 }
 
 void forward(int d){
+    setColor(255,0,0);  // RED
     servo1.write(103);
     servo2.write(84);
     servo3.write(84);
@@ -27,6 +40,7 @@ void reverse(int d){
 }
 
 void turnLeft(int d){
+    setColor(0,0,255);  // BLUE
     servo1.write(84);
     servo2.write(84);
     servo3.write(84);
